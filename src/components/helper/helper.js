71f6d4b4399ac3a666e46289bb0dde42b6465a67
rainiecao <rainiecao@outlook.com>
2017-04-22 14:40:24 +0800

    提交图片

--- src/components/helper/helper.js
+++ src/components/helper/helper.js
@@ -70,10 +70,10 @@ function getHost(){
 		case "develop":
 		case "release":
 			// return "http://www.shmlhw.com/huihuan/"
-			var port = window.location.port?(window.location.port>>0)+2:"9092";
-			return "http://192.168.2.108:"+port+"/api/";
+			// var port = window.location.port?(window.location.port>>0)+2:"9092";
+			// return "http://192.168.2.108:"+port+"/api/";
 			// return "http://112.74.48.183:8080/huihuan/";
-			// return "http://test.shmlhw.com/menglv/";
+			return "http://www.shmlhw.com/menglv/";
 		break;
 	}
 }
